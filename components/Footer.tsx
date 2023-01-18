b2d284fddeb1de7db3e0450beaa2c8e5a9b5090e
Sunny KaPatel <psunny1411@gmail.com>
2023-01-18 01:36:53 -0600

    Added tech list in footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,10 +1,19 @@
 import React from 'react'
+import { SiReact, SiTypescript, SiNextdotjs, SiBootstrap, SiVercel, SiGithub } from "react-icons/si";
 
 export default function Footer() {
   return (
     <footer id='footer'>
       <div className="footer-text">
         Designed & built by Sunny Patel
+      </div>
+      <div className="footer-tech-list">
+        <SiReact size={'1.7em'} />
+        <SiTypescript size={'1.7em'} />
+        <SiBootstrap size={'1.7em'} />
+        <SiNextdotjs size={'1.7em'} />
+        <SiVercel size={'1.7em'} />
+        <SiGithub size={'1.7em'} />
       </div>
     </footer>
   )
